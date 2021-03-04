62f94c4a1e6a3eddc4cc3023339b6a13d9f6fbb8
sagarakoudai <magnons180@gmail.com>
2021-03-04 13:52:18 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '644412d0942791be324bb2555da8cb89d7626b94059666c6b344563bf96082944caf3255972ecbc7d77aa2845f8c2fff85e3e730630c2890333063e91acac9a8'
+
